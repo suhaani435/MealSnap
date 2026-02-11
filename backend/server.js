5fc1e4ddd2dd25c96fcb2a4a12f1434b4fdbbc2b
Raphel6969 <saswat20061103@gmail.com>
2026-02-11 23:16:14 +0530

    fixed for deploy 3

--- backend/server.js
+++ backend/server.js
@@ -107,7 +107,7 @@ app.post('/api/analyze', upload.single('image'), async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
